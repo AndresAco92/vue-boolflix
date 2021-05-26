e6cc504f5b6c897e3433f39cd1b583096a3cbedc
AndresAco92 <andres.antonio.aco92@gmail.com>
2021-05-26 16:29:46 +0200

    Work in progress...

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,62 @@
+<template>
+  
+  <header>
+    <div class="left">
+      <h1>ciao</h1>
+    </div>
+    <div class="right">
+      <input placeholder="Scrivimi!" type="text">
+      <button>Cerca</button>
+    </div>
+  </header>
+
+</template>
+
+<script>
+export default {
+  name:'HeaderComp'
+}
+</script>
+
+<style lang='scss' scoped>
+
+  header{
+    height: 70px;
+    background-color: #000;
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 2px solid red;
+    .left,
+    .right{
+      height: 100%;
+      display: flex;
+      align-items: center;
+    }
+    .left{
+      padding-left: 20px;
+      color: #fff;
+      &:hover{
+        cursor: pointer;
+        transform: scale(1.1);
+      }
+    }
+    .right{
+      padding-right: 20px;
+      input:hover,
+      button:hover{
+        cursor: pointer;
+        transform: scale(1.1);
+      }
+      input{
+        margin-right: 20px;
+        border-radius: 5px;
+        color: red;
+        padding: 2px;
+      }
+      button{
+        padding: 1px 3px;
+      }
+    }
+  }
+
+</style>
---
+++ src/components/MainComp.vue
@@ -0,0 +1,47 @@
+<template>
+
+  <main>
+    <div>
+      
+    </div>
+  </main>
+
+</template>
+
+<script>
+
+  import axios from 'axios'
+  export default {
+    name:'MainComp',
+    components:{
+    },
+    data(){
+      return{
+        apiURL:'https://api.themoviedb.org/3/search/movie',
+        apiKey:'2ebbaf41573368f2518eaf78fad9226a',
+        query:'marvel',
+        cover:[]
+      }
+    },
+    mounted(){
+      axios.get(this.apiURL,{
+        params:{
+          api_key: this.apiKey,
+          query: this.query,
+          language:'it-IT'
+        }
+      })
+      .then(resp => {
+        console.log(resp.data.results);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+    }
+  }    
+  
+</script>
+
+<style>
+
+</style>
